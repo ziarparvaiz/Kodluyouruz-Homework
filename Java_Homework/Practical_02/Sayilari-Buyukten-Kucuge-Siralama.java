4c5dccde3adb14bc3208eff4e6cb08d14d33142f
ZIAR Parvaiz <ziarparvaiz@gmail.com>
2022-03-17 22:34:05 +0300

    Pratik - Sayilari-Buyukten-Kucuge-Siralama

---
+++ Java_Homework/Practical_02/Sayilari-Buyukten-Kucuge-Siralama.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] arg) {
+    Scanner input = new Scanner(System.in);
+    int a, b, c;
+
+    System.out.print("Sayi 1: ");
+    a = input.nextInt();
+
+    System.out.print("Sayi 2: ");
+    b = input.nextInt();
+
+    System.out.print("Sayi 3: ");
+    c = input.nextInt();
+
+
+    if ((a > b) && (a > c)) {
+        if (b > c) {
+            System.out.print("a > b > c");
+        } else {
+            System.out.println("a > c > b");
+        }
+    } else if ((b > a) && (b > c)) {
+        if (a > c) {
+            System.out.println("b > a > c");
+        } else  {
+            System.out.println("b > c > a");
+        }
+    } else if ((c > a) && (c > b)) {
+        if (a > b) {
+            System.out.println("c > a > b");
+        } else {
+            System.out.println("c > b > a");
+        }
+    }
+
+
+    }
+}
